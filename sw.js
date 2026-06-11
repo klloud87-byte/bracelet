834c176c53080ff2afce877c7863ee8895800334
khouloud <klloud87@gmail.com>
2026-06-11 22:49:04 +0200

    Initial commit

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'lody-v1';
+const FILES = [
+  '/lodybracelet.html',
+  '/product.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
