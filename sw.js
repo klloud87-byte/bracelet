c66df54902bccd45fbd1e48bc98d4877eb820e38
khouloud <klloud87@gmail.com>
2026-06-24 11:06:33 +0200

    Update website

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE = 'lody-v1';
+const CACHE = 'lody-v2';
 const FILES = [
   '/index.html',
+  '/Shop.html',
   '/product.html',
   '/manifest.json'
 ];
@@ -9,6 +10,18 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll(FILES))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
